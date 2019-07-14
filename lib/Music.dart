d32ddd15c3d6bcb5bb87b4db75787ec601145007
berangeresn <49882163+berangeresn@users.noreply.github.com>
2019-07-14 21:52:04 +0200

    Add lib folder with main.dart and Music.dart

---
+++ lib/Music.dart
@@ -0,0 +1,17 @@
+class Music {
+
+  String title;
+  String artist;
+  String imagePath;
+  String urlSong;
+
+  //constructeur
+
+  Music(this.title, this.artist, this.imagePath, this.urlSong) {
+    this.title = title;
+    this.artist = artist;
+    this.imagePath = imagePath;
+    this.urlSong = urlSong;
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,236 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'Music.dart';
+import 'package:audioplayer2/audioplayer2.dart';
+
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'RJ Ellory - Playlist',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'RJ Ellory - Playlist'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List<Music> maListeDeMusiques = [
+    new Music('A Dark and Broken Heart', 'RJ Ellory and The Whiskey Poets', 'assets/ellorywp.jpg', 'https://drive.google.com/file/d/1fDJseOsakKTS-JoILFh07In4GXp8K1WN/view?usp=sharing'),
+    new Music('Shelter from the Storm', 'Bob Dylan', 'assets/shelter.jpg', 'https://www.youtube.com/watch?v=-gsDBuHwqbM&list=PLjf8Vkzx4Hw66sK5wxSb3aDRMQnUQRzKl&index=2'),
+    new Music('Astral Weeks', 'Van Morrison', 'assets/astral.jpg', 'https://www.youtube.com/watch?v=4ech6pZoBJ4&list=PLjf8Vkzx4Hw66sK5wxSb3aDRMQnUQRzKl&index=3'),
+    new Music('9 Below Zero', 'Canned Heat', 'assets/below.jpg', 'https://www.youtube.com/watch?v=HxI3OS3vlCw&list=PLjf8Vkzx4Hw66sK5wxSb3aDRMQnUQRzKl&index=4'),
+    new Music('Talkin Bout A Revolution', 'Tracy Chapman', 'assets/talkin.jpg', 'https://www.youtube.com/watch?v=721JQZw6Spg&list=PLjf8Vkzx4Hw66sK5wxSb3aDRMQnUQRzKl&index=5'),
+
+  ];
+
+  AudioPlayer audioPlayer;
+  StreamSubscription positionSub;
+  StreamSubscription stateSubscription;
+  Music maMusiqueActuelle;
+  Duration position = new Duration(seconds: 0);
+  Duration duree = new Duration(seconds: 10);
+  PlayerState statut = PlayerState.stopped;
+  int index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    maMusiqueActuelle = maListeDeMusiques[index];
+    configurationAudioPlayer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.grey[900],
+        title: new Text(widget.title),
+      ),
+      backgroundColor: Colors.grey[800],
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new Card(
+              elevation: 9.0,
+              child: new Container(
+                width: MediaQuery.of(context).size.height / 2.5,
+                child: new Image.asset(maMusiqueActuelle.imagePath),
+              ),
+            ),
+            texteAvecStyle(maMusiqueActuelle.title, 1.5),
+            texteAvecStyle(maMusiqueActuelle.artist, 1.0),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                bouton(Icons.fast_rewind, 30.0, ActionMusic.rewind),
+                bouton((statut == PlayerState.playing) ?Icons.pause: Icons.play_arrow, 45.0, (statut == PlayerState.playing) ? ActionMusic.pause: ActionMusic.play),
+                bouton(Icons.fast_forward, 30.0, ActionMusic.forward)
+              ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                texteAvecStyle(fromDuration(position), 0.8),
+                texteAvecStyle(fromDuration(duree), 0.8)
+              ],
+            ),
+            new Slider(
+                value: position.inSeconds.toDouble(),
+                min: 0.0,
+                max: 30.0,
+                inactiveColor: Colors.white,
+                activeColor: Colors.amber,
+                onChanged: (double d) {
+                  setState(() {
+                    audioPlayer.seek(d);
+                  });
+                })
+          ],
+        ),
+      ),
+    );
+  }
+
+  IconButton bouton(IconData icone, double taille, ActionMusic action) {
+    return new IconButton(
+      iconSize: taille,
+      color: Colors.white,
+      icon: new Icon(icone),
+      onPressed: () {
+        switch (action) {
+          case ActionMusic.play:
+            play();
+            break;
+          case ActionMusic.pause:
+            pause();
+            break;
+          case ActionMusic.forward:
+            forward();
+            break;
+          case ActionMusic.rewind:
+            rewind();
+            break;
+        }
+      },
+    );
+  }
+
+  Text texteAvecStyle(String data, double scale) {
+    return new Text(
+      data,
+      textScaleFactor: scale,
+      textAlign: TextAlign.center,
+      style: new TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontStyle: FontStyle.italic
+      ),
+    );
+  }
+
+  void configurationAudioPlayer() {
+    audioPlayer = new AudioPlayer();
+    positionSub = audioPlayer.onAudioPositionChanged.listen(
+            (pos) => setState(() => position = pos)
+    );
+    stateSubscription = audioPlayer.onPlayerStateChanged.listen((state) {
+      if (state == AudioPlayerState.PLAYING) {
+        setState(() {
+          duree = audioPlayer.duration;
+        });
+      } else if (state == AudioPlayerState.STOPPED) {
+        setState(() {
+          statut = PlayerState.stopped;
+        });
+      }
+    }, onError: (message) {
+      print('erreur: $message');
+      setState(() {
+        statut = PlayerState.stopped;
+        duree = new Duration(seconds: 0);
+        position = new Duration(seconds: 0);
+      });
+    }
+    );
+  }
+
+  Future play() async {
+    await audioPlayer.play(maMusiqueActuelle.urlSong);
+    setState(() {
+      statut = PlayerState.playing;
+    });
+  }
+
+  Future pause() async {
+    await audioPlayer.pause();
+    setState(() {
+      statut = PlayerState.paused;
+    });
+  }
+
+  void forward() {
+    if (index == maListeDeMusiques.length - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
+    maMusiqueActuelle = maListeDeMusiques[index];
+    audioPlayer.stop();
+    configurationAudioPlayer();
+    play();
+  }
+
+  String fromDuration(Duration duree) {
+    print(duree);
+    return duree.toString().split('.').first;
+  }
+
+  void rewind() {
+    if (position > Duration(seconds: 3)) {
+      audioPlayer.seek(0.0);
+    } else {
+      if (index == 0) {
+        index = maListeDeMusiques.length - 1;
+      } else {
+        index--;
+      }
+      maMusiqueActuelle = maListeDeMusiques[index];
+      audioPlayer.stop();
+      configurationAudioPlayer();
+      play();
+    }
+  }
+}
+
+enum ActionMusic {
+  play,
+  pause,
+  rewind,
+  forward
+}
+
+enum PlayerState {
+  playing,
+  stopped,
+  paused
+}
